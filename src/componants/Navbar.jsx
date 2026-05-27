b6432b658b1f425835ba84cd7360ad7a064321f7
sachingitpreneurpune <sachin.g@itpreneurpune.com>
2026-05-27 09:58:52 +0530

    Navbar Updated

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <ul style={{display:'flex',gap:'20px',padding:'20px',background:'black' , justifyContent:"space-evenly"}}>
-            <Link to='/' style={{color:'white' , textDecoration:"none"}}>Home</Link>
+        <ul style={{display:'flex',gap:'20px',padding:'20px',background:'yellow' , justifyContent:"space-evenly"}}>
+            <Link to='/' style={{color:'red' , textDecoration:"none"}}>Home</Link>
             <Link to='/add' style={{color:'white' , textDecoration:"none"}}>Add Students</Link>
             <Link to='/student' style={{color:'white' , textDecoration:"none"}}>Student List</Link>
         </ul>
